ecce925c96365c6d5dc53aca5abdc8ed6d190daa
Taishawn King <taishawnking1@yahoo.com>
2019-11-08 19:17:40 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,7 @@
 def oxford_comma(array)
-  
-  array = array.join(" and ")
+   if array.length == 2 
+    return array.join(" and ")
+ elsif array.length >= 3
+    return array.join("," , "and")
+  end
 end
