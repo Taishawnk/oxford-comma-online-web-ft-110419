3626a300d9a5de806b98a05ba3f63ca2908d43c8
Taishawn King <taishawnking1@yahoo.com>
2019-11-08 19:15:47 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,4 @@
 def oxford_comma(array)
-  if array.length == 2 
-    puts array.join(" and ")
- elsif array.length >= 3
-    puts array.join("," , "and")
-  end
+  
+  array = array.join(" and ")
 end
